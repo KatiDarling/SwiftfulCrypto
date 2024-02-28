c40c8f82fce8b706901fc4bfe182d3bb2e312f0c
KatiDarling <kati.bicknell@gmail.com>
2024-02-28 11:21:15 -0700

    Initial Commit

---
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeView.swift
+//  SwiftfulCrypto
+//
+//  Created by Katherine Bicknell on 2/23/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Header")
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Katherine Bicknell on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
